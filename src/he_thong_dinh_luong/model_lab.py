8f5e9fa49545c3b871677cb532fd6858de44e662
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-07-31 22:59:06 +0700

    chore(model-lab): route stable entrypoint to v7

--- src/he_thong_dinh_luong/model_lab.py
+++ src/he_thong_dinh_luong/model_lab.py
@@ -1,5 +1,5 @@
 """Stable entrypoint for the predictive-value multi-model research lab."""
-from .model_lab_upgrade_v6 import main, run_model_lab
+from .model_lab_upgrade_v7 import main, run_model_lab
 
 __all__ = ["main", "run_model_lab"]
 
