30700eff6dada86c56a0c22f1f7593497563505b
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-04 14:00:50 +0700

    feat(v47): activate model calendar and signal guards

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -2,6 +2,8 @@
 
 __version__ = "0.3.0"
 
+from .model_safety import apply as _apply_model_safety
 from .performance_safety import apply as _apply_performance_safety
 
+_apply_model_safety()
 _apply_performance_safety()
