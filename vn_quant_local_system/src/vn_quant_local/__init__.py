f801050af96e45d8dbb92f992952f09f3b2ed844
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-04 16:36:31 +0700

    feat(v49): activate source integrity runtime patch

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -1,13 +1,15 @@
 """VN Quant Local Workstation package."""
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from .model_safety import apply as _apply_model_safety
 from .performance_safety import apply as _apply_performance_safety
 from .compat_v47 import apply as _apply_v47_compatibility
 from .performance_corrections import apply as _apply_performance_corrections
+from .source_integrity_v49 import apply as _apply_source_integrity
 
 _apply_model_safety()
 _apply_performance_safety()
 _apply_v47_compatibility()
 _apply_performance_corrections()
+_apply_source_integrity()
