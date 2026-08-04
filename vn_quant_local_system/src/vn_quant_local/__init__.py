240a094266fb64cf4b27886bf9d944205a45ce3f
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-04 15:19:21 +0700

    feat(v48): activate auditable performance corrections

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -1,11 +1,13 @@
 """VN Quant Local Workstation package."""
 
-__version__ = "0.3.1"
+__version__ = "0.4.0"
 
 from .model_safety import apply as _apply_model_safety
 from .performance_safety import apply as _apply_performance_safety
 from .compat_v47 import apply as _apply_v47_compatibility
+from .performance_corrections import apply as _apply_performance_corrections
 
 _apply_model_safety()
 _apply_performance_safety()
 _apply_v47_compatibility()
+_apply_performance_corrections()
