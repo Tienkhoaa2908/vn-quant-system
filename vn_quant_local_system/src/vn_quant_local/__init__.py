814ee47a9f780b978fb22c95d4dd2b92699b39dc
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-05 16:23:46 +0700

    fix(v52): activate pre-execution discard safety

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -10,6 +10,7 @@ from .source_integrity_v49 import apply as _apply_source_integrity
 from .v51_integrity import apply as _apply_v51_integrity
 from .v51_safety import apply as _apply_v51_safety
 from .v52_cycle_management import apply as _apply_v52_cycle_management
+from .v52_discard_safety import apply as _apply_v52_discard_safety
 from .v52_commands import apply as _apply_v52_commands
 
 _apply_model_safety()
@@ -20,4 +21,5 @@ _apply_source_integrity()
 _apply_v51_integrity()
 _apply_v51_safety()
 _apply_v52_cycle_management()
+_apply_v52_discard_safety()
 _apply_v52_commands()
