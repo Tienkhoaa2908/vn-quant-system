211c17e29b04616e72f984ad187e6fb747e5bce7
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-05 11:26:22 +0700

    feat(v50): activate DNSE buying-power integration

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -1,15 +1,17 @@
 """VN Quant Local Workstation package."""
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 from .model_safety import apply as _apply_model_safety
 from .performance_safety import apply as _apply_performance_safety
 from .compat_v47 import apply as _apply_v47_compatibility
 from .performance_corrections import apply as _apply_performance_corrections
 from .source_integrity_v49 import apply as _apply_source_integrity
+from .buying_power_v50 import apply as _apply_buying_power
 
 _apply_model_safety()
 _apply_performance_safety()
 _apply_v47_compatibility()
 _apply_performance_corrections()
 _apply_source_integrity()
+_apply_buying_power()
