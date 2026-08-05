86707910cf397f34425084da904665acd04a6f2c
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-05 17:17:10 +0700

    fix(v53): activate serialization safety

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -14,6 +14,7 @@ from .v52_discard_safety import apply as _apply_v52_discard_safety
 from .v52_status_safety import apply as _apply_v52_status_safety
 from .v52_commands import apply as _apply_v52_commands
 from .v53_cycle_cleanup import apply as _apply_v53_cycle_cleanup
+from .v53_safety import apply as _apply_v53_safety
 
 _apply_model_safety()
 _apply_performance_safety()
@@ -27,3 +28,4 @@ _apply_v52_discard_safety()
 _apply_v52_status_safety()
 _apply_v52_commands()
 _apply_v53_cycle_cleanup()
+_apply_v53_safety()
