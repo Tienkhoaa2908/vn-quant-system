644faf2b05ecabc757522b60322539c36a26e71e
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-05 14:42:39 +0700

    feat(v51): activate intent and cash integrity layer

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -1,15 +1,17 @@
 """VN Quant Local Workstation package."""
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 from .model_safety import apply as _apply_model_safety
 from .performance_safety import apply as _apply_performance_safety
 from .compat_v47 import apply as _apply_v47_compatibility
 from .performance_corrections import apply as _apply_performance_corrections
 from .source_integrity_v49 import apply as _apply_source_integrity
+from .v51_integrity import apply as _apply_v51_integrity
 
 _apply_model_safety()
 _apply_performance_safety()
 _apply_v47_compatibility()
 _apply_performance_corrections()
 _apply_source_integrity()
+_apply_v51_integrity()
