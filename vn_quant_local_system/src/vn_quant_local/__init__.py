e828c8db92ac220af3b386b8836b801b319d21b8
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-05 16:18:15 +0700

    feat(v52): activate cycle management runtime

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -1,6 +1,6 @@
 """VN Quant Local Workstation package."""
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 from .model_safety import apply as _apply_model_safety
 from .performance_safety import apply as _apply_performance_safety
@@ -9,6 +9,7 @@ from .performance_corrections import apply as _apply_performance_corrections
 from .source_integrity_v49 import apply as _apply_source_integrity
 from .v51_integrity import apply as _apply_v51_integrity
 from .v51_safety import apply as _apply_v51_safety
+from .v52_cycle_management import apply as _apply_v52_cycle_management
 
 _apply_model_safety()
 _apply_performance_safety()
@@ -17,3 +18,4 @@ _apply_performance_corrections()
 _apply_source_integrity()
 _apply_v51_integrity()
 _apply_v51_safety()
+_apply_v52_cycle_management()
