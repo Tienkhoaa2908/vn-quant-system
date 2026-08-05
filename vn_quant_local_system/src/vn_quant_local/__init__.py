b21c38f6c2ac955f104bfda6149f85db25bf54ca
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-05 16:19:49 +0700

    feat(v52): activate audited cycle commands

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -10,6 +10,7 @@ from .source_integrity_v49 import apply as _apply_source_integrity
 from .v51_integrity import apply as _apply_v51_integrity
 from .v51_safety import apply as _apply_v51_safety
 from .v52_cycle_management import apply as _apply_v52_cycle_management
+from .v52_commands import apply as _apply_v52_commands
 
 _apply_model_safety()
 _apply_performance_safety()
@@ -19,3 +20,4 @@ _apply_source_integrity()
 _apply_v51_integrity()
 _apply_v51_safety()
 _apply_v52_cycle_management()
+_apply_v52_commands()
