7bb9f6d616dcfb04fb7fb2c4f97715214ef3faa4
Tienkhoaa2908 <khoadao29082007@gmail.com>
2026-08-05 11:43:17 +0700

    fix(v50): apply PPSE safety layer

--- vn_quant_local_system/src/vn_quant_local/__init__.py
+++ vn_quant_local_system/src/vn_quant_local/__init__.py
@@ -8,6 +8,7 @@ from .compat_v47 import apply as _apply_v47_compatibility
 from .performance_corrections import apply as _apply_performance_corrections
 from .source_integrity_v49 import apply as _apply_source_integrity
 from .buying_power_v50 import apply as _apply_buying_power
+from .buying_power_safety_v50 import apply as _apply_buying_power_safety
 from .compat_v50 import apply as _apply_v50_compatibility
 
 _apply_model_safety()
@@ -16,4 +17,5 @@ _apply_v47_compatibility()
 _apply_performance_corrections()
 _apply_source_integrity()
 _apply_buying_power()
+_apply_buying_power_safety()
 _apply_v50_compatibility()
